85d90e4cf9d104706b70a48c3200a3c08e2b4941
dclleite <eduappsplay@gmail.com>
2020-07-28 23:29:18 -0300

    Aula 2 - imersão

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
 
+
 function Menu(){
     return (
         <nav className="Menu" >
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="DevFlix logo"/>
-            </a>
+            </Link>
 
-            <Button as="a" href="/" className="ButtonLink">
+            <Button as={Link} to="/cadastro/video" className="ButtonLink">
                 Novo video
             </Button>
         </nav>
